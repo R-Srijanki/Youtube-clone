62ea8276354888ac7bc5826ec45382513d106984
R-Srijanki <jankirathod999@gmail.com>
2025-11-21 18:36:24 +0530

    header

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -9,8 +9,26 @@ export default function Aside(){
     <aside className="w-1/5 h-screen bg-gray-100 p-4">
       <ul className="flex flex-col gap-3">
         <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
+        <li>Shorts</li>
+        <li>Subscriptions</li>
+      </ul>
+      <hr/>
+      <span>Explore</span>
+      <ul className="flex flex-col gap-3">
+          <li>Music</li>
+          <li>Movies</li>
+          <li>Gaming</li>
+          <li>News</li>
+          <li>Sports</li>
+          <li>Courses</li>
+          <li>Fashion & Beauty</li>
+      </ul>
+      <hr/>
+      <ul>
+        <li>Settings</li>
+        <li>Report History</li>
+        <li>Help</li>
+        <li>Send Feedback</li>
       </ul>
     </aside>)
             }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,11 @@
 import { useContext, useState } from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
 import asidecontext from "../Context/useContext";
+import { FaYoutube } from "react-icons/fa";
+import { IoSearchOutline } from "react-icons/io5";
+import { MdVideoCameraFront } from "react-icons/md";
+import { IoIosNotificationsOutline } from "react-icons/io";
+import { CgProfile } from "react-icons/cg";
 export default function Header(){
     const {visible,setvisible}=useContext(asidecontext);
     function handleclick(){
@@ -8,9 +13,25 @@ export default function Header(){
     }
     return(
         <div>
-            <header className="flex justify-between w-screen">
-                <button onClick={handleclick}><RxHamburgerMenu/></button>
-                <h1>Youtube</h1>
+            <header className="flex justify-between w-screen px-5 py-2.5">
+                <div className="flex">
+                    <button onClick={handleclick}><RxHamburgerMenu/></button>
+                    <div className="flex ml-5">
+                        <span className="text-red-500 mt-1 text-2xl"><FaYoutube/></span>
+                        <p className="text-xl font-bold">YouTube</p>
+                    </div>
+                </div>
+                <div className="flex">
+                    <div className="flex border rounded-xl w-[400px]">
+                        <input type="text" placeholder="Search" className="outline-none w-[375px] px-2 py-1"/>
+                        <span className="w-[25x] border-l flex items-center justify-center"><IoSearchOutline/></span>
+                    </div>
+                </div>
+                <div className="flex justify-evenly">
+                   <span className="text-2xl mx-5"><MdVideoCameraFront/></span>
+                   <span className="text-2xl mx-2.5"><IoIosNotificationsOutline/></span>
+                   <span className="text-2xl mx-2.5"><CgProfile/></span>
+                </div>
             </header>
         </div>
     )
